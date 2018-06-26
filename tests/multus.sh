ed5b11e0f8126392261b843384fd9b9c9bf9162b
Victor Morales <victor.morales@intel.com>
2018-06-26 04:53:26 -0700

    Create draft for testing multus networks

--- tests/multus.sh
+++ tests/multus.sh
@@ -12,4 +12,61 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Not implemented"
+cat << MULTUSNET01 >> $HOME/flannel-network.yaml
+apiVersion: "kubernetes.com/v1"
+kind: Network
+metadata:
+  name: flannel-conf
+plugin: flannel
+args: '[
+        {
+                "delegate": {
+                        "isDefaultGateway": true
+                }
+        }
+]'
+MULTUSNET01
+
+cat << MULTUSNET02 >> $HOME/flannel-network2.yaml
+apiVersion: "kubernetes.com/v1"
+kind: Network
+metadata:
+  name: flannel-conf2
+plugin: flannel
+args: '[
+        {
+                "delegate": {
+                        "isDefaultGateway": true
+                }
+        }
+]'
+MULTUSNET02
+
+cat << MULTUSPOD >> $HOME/pod-multi-network.yaml
+apiVersion: v1
+kind: Pod
+metadata:
+  name: multus-multi-net-pod
+  annotations:
+    networks: '[
+        { "name": "flannel-conf" },
+        { "name": "flannel-conf2" },
+    ]'
+spec:  # specification of the pod's contents
+  containers:
+  - name: multus-multi-net-pod
+    image: "busybox"
+    command: ["top"]
+    stdin: true
+    tty: true
+MULTUSPOD
+
+if $(kubectl version &>/dev/null); then
+    kubectl create -f $HOME/flannel-network.yaml
+    kubectl create -f $HOME/flannel-network2.yaml
+    kubectl create -f $HOME/pod-multi-network.yaml
+
+    #kubectl get pods --all-namespaces -o wide -w
+
+    #kubectl get pod multus-multi-net-pod
+fi
