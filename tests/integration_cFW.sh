68f9b5fc73e17aac9496a6fc328db39a2f38baf6
Victor Morales <victor.morales@intel.com>
2018-08-09 01:01:29 -0700

    Add integration tests for CNFs

--- tests/integration_cFW.sh
+++ tests/integration_cFW.sh
@@ -16,12 +16,6 @@ rm -f $HOME/*.yaml
 packetgen_deployment_name=packetgen
 sink_deployment_name=sink
 firewall_deployment_name=firewall
-image_name=virtlet.cloud/ubuntu/16.04
-
-if [[ ! -f $HOME/.ssh/id_rsa.pub ]]; then
-    echo -e "\n\n\n" | ssh-keygen -t rsa -N ""
-fi
-ssh_key=$(cat $HOME/.ssh/id_rsa.pub)
 
 cat << NET > $HOME/unprotected-private-net-cidr-network.yaml
 apiVersion: "kubernetes.cni.cncf.io/v1"
@@ -71,24 +65,6 @@ spec:
 }'
 NET
 
-proxy="#!/bin/bash"
-if [[ -n "${http_proxy+x}" ]]; then
-    proxy+="
-            export http_proxy=$http_proxy
-            echo \"Acquire::http::Proxy \\\"$http_proxy\\\";\" | sudo tee --append /etc/apt/apt.conf.d/01proxy
-"
-fi
-if [[ -n "${https_proxy+x}" ]]; then
-    proxy+="
-            export https_proxy=$https_proxy
-            echo \"Acquire::https::Proxy \\\"$https_proxy\\\";\" | sudo tee --append /etc/apt/apt.conf.d/01proxy
-"
-fi
-if [[ -n "${no_proxy+x}" ]]; then
-    proxy+="
-            export no_proxy=$no_proxy"
-fi
-
 cat << DEPLOYMENT > $HOME/$packetgen_deployment_name.yaml
 apiVersion: apps/v1
 kind: Deployment
@@ -106,37 +82,14 @@ spec:
       labels:
         app: vFirewall
       annotations:
-        VirtletCloudInitUserData: |
-          users:
-          - default
-          - name: admin
-            sudo: ALL=(ALL) NOPASSWD:ALL
-            plain_text_passwd: secret
-            groups: sudo
-            ssh_authorized_keys:
-            - $ssh_key
-        VirtletCloudInitUserDataScript: |
-            $proxy
-
-            wget -O - https://raw.githubusercontent.com/electrocucaracha/vFW-demo/master/$packetgen_deployment_name | sudo -E bash
         kubernetes.v1.cni.cncf.io/networks: '[
             { "name": "unprotected-private-net-cidr", "interfaceRequest": "eth1" },
             { "name": "onap-private-net-cidr", "interfaceRequest": "eth2" }
         ]'
-        kubernetes.io/target-runtime: virtlet.cloud
     spec:
-      affinity:
-        nodeAffinity:
-          requiredDuringSchedulingIgnoredDuringExecution:
-            nodeSelectorTerms:
-            - matchExpressions:
-              - key: extraRuntime
-                operator: In
-                values:
-                - virtlet
       containers:
       - name: $packetgen_deployment_name
-        image: $image_name
+        image: electrocucaracha/packetgen
         imagePullPolicy: IfNotPresent
         tty: true
         stdin: true
@@ -162,38 +115,15 @@ spec:
       labels:
         app: vFirewall
       annotations:
-        VirtletCloudInitUserData: |
-          users:
-          - default
-          - name: admin
-            sudo: ALL=(ALL) NOPASSWD:ALL
-            plain_text_passwd: secret
-            groups: sudo
-            ssh_authorized_keys:
-            - $ssh_key
-        VirtletCloudInitUserDataScript: |
-            $proxy
-
-            wget -O - https://raw.githubusercontent.com/electrocucaracha/vFW-demo/master/$firewall_deployment_name | sudo -E bash
         kubernetes.v1.cni.cncf.io/networks: '[
             { "name": "unprotected-private-net-cidr", "interfaceRequest": "eth1" },
             { "name": "protected-private-net-cidr", "interfaceRequest": "eth2" },
             { "name": "onap-private-net-cidr", "interfaceRequest": "eth3" }
         ]'
-        kubernetes.io/target-runtime: virtlet.cloud
     spec:
-      affinity:
-        nodeAffinity:
-          requiredDuringSchedulingIgnoredDuringExecution:
-            nodeSelectorTerms:
-            - matchExpressions:
-              - key: extraRuntime
-                operator: In
-                values:
-                - virtlet
       containers:
       - name: $firewall_deployment_name
-        image: $image_name
+        image: electrocucaracha/firewall
         imagePullPolicy: IfNotPresent
         tty: true
         stdin: true
@@ -219,37 +149,14 @@ spec:
       labels:
         app: vFirewall
       annotations:
-        VirtletCloudInitUserData: |
-          users:
-          - default
-          - name: admin
-            sudo: ALL=(ALL) NOPASSWD:ALL
-            plain_text_passwd: secret
-            groups: sudo
-            ssh_authorized_keys:
-            - $ssh_key
-        VirtletCloudInitUserDataScript: |
-            $proxy
-
-            wget -O - https://raw.githubusercontent.com/electrocucaracha/vFW-demo/master/$sink_deployment_name | sudo -E bash
         kubernetes.v1.cni.cncf.io/networks: '[
             { "name": "protected-private-net-cidr", "interfaceRequest": "eth1" },
             { "name": "onap-private-net-cidr", "interfaceRequest": "eth2" }
         ]'
-        kubernetes.io/target-runtime: virtlet.cloud
     spec:
-      affinity:
-        nodeAffinity:
-          requiredDuringSchedulingIgnoredDuringExecution:
-            nodeSelectorTerms:
-            - matchExpressions:
-              - key: extraRuntime
-                operator: In
-                values:
-                - virtlet
       containers:
       - name: $sink_deployment_name
-        image: $image_name
+        image: electrocucaracha/sink
         imagePullPolicy: IfNotPresent
         tty: true
         stdin: true
@@ -283,13 +190,5 @@ if $(kubectl version &>/dev/null); then
                 exit 1
             fi
         done
-    done
-    for deployment_name in $packetgen_deployment_name $firewall_deployment_name $sink_deployment_name; do
-        pod_name=$(kubectl get pods | grep  $deployment_name | awk '{print $1}')
-        vm=$(kubectl plugin virt virsh list | grep ".*$deployment_name"  | awk '{print $2}')
-        echo "Pod name: $pod_name Virsh domain: $vm"
-        echo "ssh -i ~/.ssh/id_rsa.pub admin@$(kubectl get pods $pod_name -o jsonpath="{.status.podIP}")"
-        echo "=== Virtlet details ===="
-        echo "$(kubectl plugin virt virsh dumpxml $vm | grep VIRTLET_)\n"
     done
 fi
