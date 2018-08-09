973836a72f746bd5f67189331c1ba1618d02eb0b
Victor Morales <victor.morales@intel.com>
2018-08-09 01:38:17 -0700

    Add deployment objects in multus tests

--- tests/multus.sh
+++ tests/multus.sh
@@ -14,9 +14,10 @@ set -o pipefail
 
 rm -f $HOME/*.yaml
 
-pod_name=multus-multi-net-pod
+pod_name=multus-pod
+deployment_name=multus-deployment
 
-cat << MULTUSNET01 > $HOME/bridge-network.yaml
+cat << NET > $HOME/bridge-network.yaml
 apiVersion: "kubernetes.cni.cncf.io/v1"
 kind: Network
 metadata:
@@ -30,51 +31,92 @@ spec:
         "subnet": "10.10.0.0/16"
     }
 }'
-MULTUSNET01
+NET
 
-cat << MULTUSPOD > $HOME/pod-multi-network.yaml
+cat << POD > $HOME/$pod_name.yaml
 apiVersion: v1
 kind: Pod
 metadata:
   name: $pod_name
   annotations:
     kubernetes.v1.cni.cncf.io/networks: '[
-        { "name": "bridge-conf", "interfaceRequest": "eth1" },
-        { "name": "bridge-conf", "interfaceRequest": "eth2" }
+      { "name": "bridge-conf", "interfaceRequest": "eth1" },
+      { "name": "bridge-conf", "interfaceRequest": "eth2" }
     ]'
 spec:  # specification of the pod's contents
   containers:
-  - name: multus-multi-net-pod
+  - name: $pod_name
     image: "busybox"
     command: ["top"]
     stdin: true
     tty: true
-MULTUSPOD
+POD
+
+cat << DEPLOYMENT > $HOME/$deployment_name.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: $deployment_name
+  labels:
+    app: multus
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: multus
+  template:
+    metadata:
+      labels:
+        app: multus
+      annotations:
+        kubernetes.v1.cni.cncf.io/networks: '[
+          { "name": "bridge-conf", "interfaceRequest": "eth1" },
+          { "name": "bridge-conf", "interfaceRequest": "eth2" }
+        ]'
+    spec:
+      containers:
+      - name: $deployment_name
+        image: "busybox"
+        command: ["top"]
+        stdin: true
+        tty: true
+DEPLOYMENT
 
 if $(kubectl version &>/dev/null); then
     kubectl apply -f $HOME/bridge-network.yaml
 
     kubectl delete pod $pod_name --ignore-not-found=true --now
+    kubectl delete deployment $deployment_name --ignore-not-found=true --now
     while kubectl get pod $pod_name &>/dev/null; do
         sleep 5
     done
-    kubectl create -f $HOME/pod-multi-network.yaml
+    kubectl create -f $HOME/$pod_name.yaml
+    while kubectl get deployment $deployment_name &>/dev/null; do
+        sleep 5
+    done
+    kubectl create -f $HOME/$deployment_name.yaml
 
-    status_phase=""
-    while [[ $status_phase != "Running" ]]; do
-        new_phase=$(kubectl get pods $pod_name | awk 'NR==2{print $3}')
-        if [[ $new_phase != $status_phase ]]; then
-            echo "$(date +%H:%M:%S) - $new_phase"
-            status_phase=$new_phase
-        fi
-        if [[ $new_phase == "Err"* ]]; then
+    deployment_pod=$(kubectl get pods | grep  $deployment_name | awk '{print $1}')
+    for pod in $pod_name $deployment_pod; do
+        status_phase=""
+        while [[ $status_phase != "Running" ]]; do
+            new_phase=$(kubectl get pods $pod | awk 'NR==2{print $3}')
+            if [[ $new_phase != $status_phase ]]; then
+                echo "$(date +%H:%M:%S) - $pod : $new_phase"
+                status_phase=$new_phase
+            fi
+            if [[ $new_phase == "Err"* ]]; then
+                exit 1
+            fi
+        done
+    done
+
+    for pod in $pod_name $deployment_pod; do
+        echo "===== $pod details ====="
+        kubectl exec -it $pod -- ip a
+        multus_nic=$(kubectl exec -it $pod -- ifconfig | grep "eth1")
+        if [ -z "$multus_nic" ]; then
             exit 1
         fi
     done
-
-    kubectl exec -it $pod_name -- ip a
-    multus_nic=$(kubectl exec -it $pod_name -- ifconfig | grep "eth1")
-    if [ -z "$multus_nic" ]; then
-        exit 1
-    fi
 fi
