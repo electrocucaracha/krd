02f9808259e957c4523f078935444fb83da8a8bf
Victor Morales <victor.morales@intel.com>
2019-03-22 18:00:44 -0700

    Fix kernel dependencies during docker installation

--- _installers.sh
+++ _installers.sh
@@ -47,7 +47,7 @@ function _install_docker {
         *suse)
         ;;
         ubuntu|debian)
-            install_packages software-properties-common linux-image-extra-$(uname -r) linux-image-extra-virtual apt-transport-https ca-certificates curl
+            install_packages apt-transport-https ca-certificates curl
             curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
             sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
             update_repos
