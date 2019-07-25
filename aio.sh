ddd34815140a89c80a2cedcf371df2a95c324394
Victor Morales <victor.morales@intel.com>
2019-07-25 16:51:15 -0700

    Update All-in-One instructions

--- aio.sh
+++ aio.sh
@@ -12,15 +12,35 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-if [[ $(whoami) == 'root' ]];then
-    echo "This bash script has been executed as root user"
+if [[ "${KRD_DEBUG:-false}" == "true" ]]; then
+    set -o xtrace
+fi
+
+if ! sudo -n "true"; then
+    echo ""
+    echo "passwordless sudo is needed for '$(id -nu)' user."
+    echo "Please fix your /etc/sudoers file. You likely want an"
+    echo "entry like the following one..."
+    echo ""
+    echo "$(id -nu) ALL=(ALL) NOPASSWD: ALL"
     exit 1
 fi
 
+# Setup SSH keys
+rm -f ~/.ssh/id_rsa*
+sudo mkdir -p /root/.ssh/
+echo -e "\n\n\n" | ssh-keygen -t rsa -N ""
+sudo cp ~/.ssh/id_rsa /root/.ssh/id_rsa
+< ~/.ssh/id_rsa.pub tee --append  ~/.ssh/authorized_keys | sudo tee --append /root/.ssh/authorized_keys
+chmod og-wx ~/.ssh/authorized_keys
+
 echo "Cloning and configuring KRD project..."
-git clone https://github.com/electrocucaracha/krd
-cd krd/
-cat <<EOL > inventory/hosts.ini
+if [ ! -d "${KRD_FOLDER:-/opt/krd}" ]; then
+    sudo git clone --depth 1 https://github.com/electrocucaracha/krd "${KRD_FOLDER:-/opt/krd}"
+    sudo chown -R "$USER" "${KRD_FOLDER:-/opt/krd}"
+fi
+cd /opt/krd || exit
+sudo tee inventory/hosts.ini << EOL
 [all]
 localhost
 
@@ -46,12 +66,9 @@ localhost
 kube-node
 kube-master
 EOL
-echo -e "\n\n\n" | ssh-keygen -t rsa -N ""
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-chmod og-wx ~/.ssh/authorized_keys
 
 echo "Enabling nested-virtualization"
-./node.sh
+sudo ./node.sh
 
 echo "Deploying KRD project"
-./installer.sh | tee krd_installer.log
+./krd_command.sh -a install_k8s | tee krd_init.log
