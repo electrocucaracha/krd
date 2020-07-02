00b211a0ca14ea92757e5e97928876c6ed3fb8fa
Victor Morales <v.morales@samsung.com>
2020-07-01 17:43:04 -0700

    Implement Ansible Molecule on PMEM role

---
+++ playbooks/roles/pmem/molecule/default/tests/test_default.py
@@ -0,0 +1,49 @@
+#   Copyright 2020
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import os
+import time
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_pmem_device_plugin_ready(host):
+    cmd = host.run("/usr/local/bin/kubectl rollout status"
+                   " daemonset/pmem-csi-node")
+
+    assert cmd.rc == 0
+    assert "successfully rolled out" in cmd.stdout
+
+def test_pmem_statefulset_ready(host):
+    cmd = host.run("/usr/local/bin/kubectl rollout status"
+                   " statefulset/pmem-csi-controller")
+
+    assert cmd.rc == 0
+    assert "partitioned roll out complete" in cmd.stdout
+
+def test_get_pmem_node_annotation(host):
+    host.run("/usr/local/bin/kubectl wait --for=condition=ready node/molecule-control-plane --timeout=120s")
+    host.run("/usr/local/bin/kubectl rollout status daemonset/pmem-csi-node")
+    host.run("/usr/local/bin/kubectl rollout status statefulset/pmem-csi-controller")
+    time.sleep(10)
+
+    jsonpath = r'{range .items[*]}{.metadata.annotations.csi\.volume\.kubernetes\.io/nodeid}'
+    cmd = host.run("/usr/local/bin/kubectl get nodes"
+                   " -o jsonpath='"+jsonpath+"'")
+
+    assert "pmem-csi.intel.com" in cmd.stdout
