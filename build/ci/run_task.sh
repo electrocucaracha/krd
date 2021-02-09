f5c2bd6747e10ee876593559c3e099c8377e06d1
Victor Morales <chipahuac@hotmail.com>
2021-02-09 10:27:06 -0800

    Add Concourse CI instructions

---
+++ build/ci/run_task.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c)
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o xtrace
+
+if ! command -v fly; then
+    # NOTE: Shorten link -> https://github.com/electrocucaracha/pkg-mgr_scripts
+    curl -fsSL http://bit.ly/install_pkg | PKG=fly bash
+fi
+
+fly execute --privileged \
+    --target="${RELENG_TARGET:-releng}" \
+    --config="${TASK_NAME}-task.yml" \
+    -i src=../../ "$@"
