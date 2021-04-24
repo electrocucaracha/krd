2457213641a051494ba5b095a73484afbe2ec08d
Victor Morales <chipahuac@hotmail.com>
2021-04-23 17:15:36 -0700

    Improve multus test script

--- tests/multus.sh
+++ tests/multus.sh
@@ -19,48 +19,16 @@ multus_deployment_name=multus-test
 bridge_net_name=bridge-test
 subnet_prefix="10.10.10"
 subnet="${subnet_prefix}.0/24"
-cni_version="0.4.0"
-container_spec=$(cat <<EOF
-    spec:
-      containers:
-        - name: instance
-          image: busybox
-          command:
-            - sleep
-          args:
-            - infinity
-EOF
-)
 
 function cleanup {
     kubectl delete network-attachment-definitions.k8s.cni.cncf.io -A --selector=app.kubernetes.io/name=multus
     destroy_deployment "$multus_deployment_name"
 }
 
-trap cleanup EXIT
+function create_deployment {
+    local net_definition=$1
 
-# Test
-info "===== Test started ====="
-
-info "+++++ Multiple Network Interfaces validation:"
-cat <<EOF | kubectl apply -f -
-apiVersion: "k8s.cni.cncf.io/v1"
-kind: NetworkAttachmentDefinition
-metadata:
-  name: $bridge_net_name
-  labels:
-    app.kubernetes.io/name: multus
-spec:
-  config: '{
-    "cniVersion": "$cni_version",
-    "name": "bridgenet",
-    "type": "bridge",
-    "ipam": {
-        "type": "host-local",
-        "subnet": "$subnet"
-    }
-}'
----
+    cat <<EOF | kubectl apply -f -
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -77,23 +45,41 @@ spec:
       labels:
         app.kubernetes.io/name: multus
       annotations:
-        k8s.v1.cni.cncf.io/networks: '[
-          { "name": "$bridge_net_name", "interfaceRequest": "eth1" },
-          { "name": "$bridge_net_name", "interfaceRequest": "eth2" }
-        ]'
-$container_spec
+$net_definition
+    spec:
+      containers:
+        - name: instance
+          image: busybox
+          command:
+            - sleep
+          args:
+            - "120"
 EOF
-wait_deployment "$multus_deployment_name"
-deployment_pod=$(kubectl get pods -l=app.kubernetes.io/name=multus -o jsonpath='{.items[0].metadata.name}')
-info "$deployment_pod details:"
-kubectl exec -it "$deployment_pod" -- ip link
-info "$deployment_pod assertions:"
-assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth1)" "$deployment_pod pod doesn't contain eth1 nic"
-assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth2)" "$deployment_pod pod doesn't contain eth2 nic"
-destroy_deployment "$multus_deployment_name"
+    wait_deployment "$multus_deployment_name"
+}
 
-info "+++++ Default Network Interfaces validation:"
+trap cleanup EXIT
+trap get_status ERR
+
+# Setup
 cat <<EOF | kubectl apply -f -
+apiVersion: "k8s.cni.cncf.io/v1"
+kind: NetworkAttachmentDefinition
+metadata:
+  name: $bridge_net_name
+  labels:
+    app.kubernetes.io/name: multus
+spec:
+  config: '{
+    "cniVersion": "0.4.0",
+    "name": "bridgenet",
+    "type": "bridge",
+    "ipam": {
+        "type": "host-local",
+        "subnet": "$subnet"
+    }
+}'
+---
 apiVersion: "k8s.cni.cncf.io/v1"
 kind: NetworkAttachmentDefinition
 metadata:
@@ -103,7 +89,7 @@ metadata:
     app.kubernetes.io/name: multus
 spec:
   config: '{
-    "cniVersion": "$cni_version",
+    "cniVersion": "0.4.0",
     "name": "bridgenet",
     "type": "bridge",
     "ipam": {
@@ -111,27 +97,36 @@ spec:
         "subnet": "$subnet"
     }
 }'
----
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: $multus_deployment_name
-  labels:
-    app.kubernetes.io/name: multus
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app.kubernetes.io/name: multus
-  template:
-    metadata:
-      labels:
-        app.kubernetes.io/name: multus
-      annotations:
-        v1.multus-cni.io/default-network: $bridge_net_name
-$container_spec
 EOF
-wait_deployment "$multus_deployment_name"
+
+# Test
+info "===== Test started ====="
+
+info "+++++ Multiple Network Interfaces validation:"
+net_definition=$(cat <<EOF
+        k8s.v1.cni.cncf.io/networks: '[
+          { "name": "$bridge_net_name", "interfaceRequest": "eth1" },
+          { "name": "$bridge_net_name", "interfaceRequest": "eth2" }
+        ]'
+EOF
+)
+create_deployment "$net_definition"
+deployment_pod=$(kubectl get pods -l=app.kubernetes.io/name=multus -o jsonpath='{.items[0].metadata.name}')
+info "$deployment_pod details:"
+kubectl exec -it "$deployment_pod" -- ip link
+info "$deployment_pod assertions:"
+assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth1)" "$deployment_pod pod doesn't contain eth1 nic"
+assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth1 | awk '/inet addr/{print substr($2,6)}' | grep "$subnet_prefix" )" "$deployment_pod pod eth1 ip doesn't belong to $bridge_net_name network"
+assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth2)" "$deployment_pod pod doesn't contain eth2 nic"
+assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth2 | awk '/inet addr/{print substr($2,6)}' | grep "$subnet_prefix" )" "$deployment_pod pod eth2 ip doesn't belong to $bridge_net_name network"
+destroy_deployment "$multus_deployment_name"
+
+info "+++++ Default Network Interfaces validation:"
+net_definition=$(cat <<EOF
+        v1.multus-cni.io/default-network: $bridge_net_name
+EOF
+)
+create_deployment "$net_definition"
 deployment_pod=$(kubectl get pods -l=app.kubernetes.io/name=multus -o jsonpath='{.items[0].metadata.name}')
 info "$deployment_pod details:"
 kubectl exec -it "$deployment_pod" -- ip link
