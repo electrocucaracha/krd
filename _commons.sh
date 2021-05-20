de225b6ea31b90921fa54cfdef8cf3ee79475c10
Victor Morales <chipahuac@hotmail.com>
2021-05-19 20:02:20 -0700

    Move install_metallb func to commons

--- _commons.sh
+++ _commons.sh
@@ -144,6 +144,55 @@ EOF
     fi
 }
 
+# install_metallb() - Install MetalLB services
+function install_metallb {
+    metallb_version=$(_get_version metallb)
+    if [ -z "${KRD_METALLB_ADDRESS_POOLS:-}" ]; then
+        declare -A KRD_METALLB_ADDRESS_POOLS=(
+    ["default"]="10.10.16.110-10.10.16.120,10.10.16.240-10.10.16.250"
+    )
+    fi
+
+    if ! kubectl get namespaces/metallb-system --no-headers -o custom-columns=name:.metadata.name; then
+        kubectl apply -f "https://raw.githubusercontent.com/metallb/metallb/$metallb_version/manifests/namespace.yaml"
+        kubectl apply -f "https://raw.githubusercontent.com/metallb/metallb/$metallb_version/manifests/metallb.yaml"
+        if ! kubectl get secret/memberlist -n metallb-system --no-headers -o custom-columns=name:.metadata.name; then
+            kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+        fi
+    fi
+
+    wait_for_pods metallb-system
+
+    address_pools=""
+    for pool in "${!KRD_METALLB_ADDRESS_POOLS[@]}"; do
+        ranges=""
+        addresses=${KRD_METALLB_ADDRESS_POOLS[$pool]}
+        for range in ${addresses//,/ }; do
+            ranges+="          - ${range}\n"
+        done
+
+        address_pools+=$(cat <<EOF
+      - name: $pool
+        protocol: layer2
+        addresses:
+$(printf '%b' "$ranges")
+EOF
+)
+    done
+
+   cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  namespace: metallb-system
+  name: config
+data:
+  config: |
+    address-pools:
+$address_pools
+EOF
+}
+
 # update_repos() - Function that updates linux repositories
 function update_repos {
     curl -fsSL http://bit.ly/install_pkg | PKG_UPDATE="true" bash
