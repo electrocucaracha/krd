6cb37892b8eb1fd87675939c9de2a271781aca14
Victor Morales <chipahuac@hotmail.com>
2022-01-04 18:27:41 -0800

    Split deployments in multus tests

--- tests/multus.sh
+++ tests/multus.sh
@@ -17,35 +17,36 @@ source _functions.sh
 # shellcheck source=tests/_assertions.sh
 source _assertions.sh
 
-multus_deployment_name=multus-test
 bridge_net_name=bridge-test
 subnet_prefix="10.10.10"
 subnet="${subnet_prefix}.0/24"
 
 function cleanup {
     kubectl delete network-attachment-definitions.k8s.cni.cncf.io -A --selector=app.kubernetes.io/name=multus
-    destroy_deployment "$multus_deployment_name"
+    destroy_deployment multinet
+    destroy_deployment defaultnet
 }
 
 function create_deployment {
-    local net_definition=$1
+    local name=$1
+    local net_definition=$2
 
     cat <<EOF | kubectl apply -f -
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  name: $multus_deployment_name
+  name: $name
   labels:
-    app.kubernetes.io/name: multus
+    app.kubernetes.io/name: $name
 spec:
   replicas: 1
   selector:
     matchLabels:
-      app.kubernetes.io/name: multus
+      app.kubernetes.io/name: $name
   template:
     metadata:
       labels:
-        app.kubernetes.io/name: multus
+        app.kubernetes.io/name: $name
       annotations:
 $net_definition
     spec:
@@ -57,7 +58,7 @@ $net_definition
           args:
             - "120"
 EOF
-    wait_deployment "$multus_deployment_name"
+    wait_deployment "$name"
 }
 
 trap cleanup EXIT
@@ -112,8 +113,8 @@ net_definition=$(cat <<EOF
         ]'
 EOF
 )
-create_deployment "$net_definition"
-deployment_pod=$(kubectl get pods -l=app.kubernetes.io/name=multus -o jsonpath='{.items[0].metadata.name}')
+create_deployment multinet "$net_definition"
+deployment_pod=$(kubectl get pods -l=app.kubernetes.io/name=multinet -o jsonpath='{.items[0].metadata.name}')
 info "$deployment_pod details:"
 kubectl exec -it "$deployment_pod" -- ip link
 info "$deployment_pod assertions:"
@@ -121,20 +122,18 @@ assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth1)" "$depl
 assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth1 | awk '/inet addr/{print substr($2,6)}' | grep "$subnet_prefix" )" "$deployment_pod pod eth1 ip doesn't belong to $bridge_net_name network"
 assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth2)" "$deployment_pod pod doesn't contain eth2 nic"
 assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth2 | awk '/inet addr/{print substr($2,6)}' | grep "$subnet_prefix" )" "$deployment_pod pod eth2 ip doesn't belong to $bridge_net_name network"
-destroy_deployment "$multus_deployment_name"
 
 info "+++++ Default Network Interfaces validation:"
 net_definition=$(cat <<EOF
         v1.multus-cni.io/default-network: $bridge_net_name
 EOF
 )
-create_deployment "$net_definition"
-deployment_pod=$(kubectl get pods -l=app.kubernetes.io/name=multus -o jsonpath='{.items[0].metadata.name}')
+create_deployment defaultnet "$net_definition"
+deployment_pod=$(kubectl get pods -l=app.kubernetes.io/name=defaultnet -o jsonpath='{.items[0].metadata.name}')
 info "$deployment_pod details:"
 kubectl exec -it "$deployment_pod" -- ip link
 info "$deployment_pod assertions:"
 assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth0)" "$deployment_pod pod doesn't contain eth0 nic"
 assert_non_empty "$(kubectl exec -it "$deployment_pod" -- ifconfig eth0 | awk '/inet addr/{print substr($2,6)}' | grep "$subnet_prefix" )" "$deployment_pod pod ip doesn't belong to $bridge_net_name network"
-destroy_deployment "$multus_deployment_name"
 
 info "===== Test completed ====="
