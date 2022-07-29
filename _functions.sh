8d58a25a5260806d04ff24b5b6d6a14e5cc9901f
Victor Morales <chipahuac@hotmail.com>
2022-07-29 14:13:46 -0700

    Refactor network benchmark functions

--- _functions.sh
+++ _functions.sh
@@ -62,6 +62,7 @@ function run_k8s_iperf {
 
     # Wait for stabilization
     wait_for_pods iperf3
+    trap '_delete_namespace iperf3' RETURN
 
     # Perform bechmarking
     kubectl get nodes -o wide | tee  "$HOME/iperf3-${KRD_NETWORK_PLUGIN}-${KRD_KUBE_PROXY_MODE}.log"
@@ -70,9 +71,6 @@ function run_k8s_iperf {
         ip=$(kubectl get pod "$pod" -n iperf3 -o jsonpath='{.status.hostIP}')
         bash -c "kubectl exec -it $pod -n iperf3 -- iperf3 -c iperf3-server -T \"Client on $ip\"" | tee --append "$HOME/iperf3-${KRD_NETWORK_PLUGIN}-${KRD_KUBE_PROXY_MODE}.log"
     done
-
-    # Clean up
-    _delete_namespace iperf3
 }
 
 function _setup_demo_app {
@@ -92,6 +90,7 @@ function _setup_demo_app {
 function run_internal_k6 {
     # Setup
     _setup_demo_app k6
+    trap '_delete_namespace k6' RETURN
     kubectl apply -f resources/k6.yml -n k6
     kubectl wait --for=condition=complete job client -n k6 --timeout=3m
 
@@ -100,9 +99,6 @@ function run_internal_k6 {
     kubectl get nodes -o wide | tee "$HOME/k6-${KRD_NETWORK_PLUGIN}-${KRD_KUBE_PROXY_MODE}-${KRD_KUBE_PROXY_SCHEDULER}.log"
     kubectl get deployments/http-server-deployment -n k6 -o wide | tee --append "$HOME/k6-${KRD_NETWORK_PLUGIN}-${KRD_KUBE_PROXY_MODE}-${KRD_KUBE_PROXY_SCHEDULER}.log"
     kubectl logs -n k6 "$pod_name" | tail -n 19 | tee --append "$HOME/k6-${KRD_NETWORK_PLUGIN}-${KRD_KUBE_PROXY_MODE}-${KRD_KUBE_PROXY_SCHEDULER}.log"
-
-    # Clean up
-    _delete_namespace k6
 }
 
 # run_external_k6() - Function that execute performance HTTP benchmark to the cluster
@@ -111,6 +107,7 @@ function run_external_k6 {
 
     # Setup
     _setup_demo_app k6
+    trap '_delete_namespace k6' RETURN
     KUBE_EDITOR='sed -i "s|  type\: .*|  type\: LoadBalancer|g"' kubectl edit svc test -n k6
     until [ -n "$(kubectl get service test -o jsonpath='{.status.loadBalancer.ingress[0].ip}' -n k6)" ]; do
         sleep 1
@@ -146,9 +143,6 @@ EOF
     kubectl get nodes -o wide | tee "$HOME/k6-${default_ingress_class}.log"
     kubectl get deployments/http-server-deployment -n k6 -o wide | tee --append "$HOME/k6-${default_ingress_class}.log"
     docker logs k6 | tail -n 19 | tee --append "$HOME/k6-${default_ingress_class}.log"
-
-    # Clean up
-    _delete_namespace k6
 }
 
 # wait_for_pods() - Function that waits for the running state
@@ -224,6 +218,7 @@ function run_checkov {
     kubectl delete -f resources/checkov-job.yaml
 }
 
+# run_kubent() - Installs Kubent to determine deprecated APIs
 function run_kubent {
     if ! command -v kubent > /dev/null; then
         curl -sSL https://git.io/install-kubent | GREEN=x TERM=dumb NOCOL=1 YELLOW=x sh
