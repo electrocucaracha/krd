7ab466a2a67d8b7253d17d5adf369b575cd6993c
Victor Morales <chipahuac@hotmail.com>
2024-06-18 18:59:48 -0700

    Fix linting issues

--- ci/update_versions.sh
+++ ci/update_versions.sh
@@ -227,5 +227,6 @@ sed -i "s/version: .*/version: v$(get_version github_release k8sgpt-ai/k8sgpt)/g
 gh_actions=$(grep -r "uses: [a-z\-]*/[a-z\-]*@" .github/workflows/ | sed 's/@.*//' | awk -F ': ' '{ print $3 }' | sort | uniq)
 for action in $gh_actions; do
     commit_hash=$(git ls-remote --tags "https://github.com/$action" | awk '{ print $NF,$0 }' | sort -k1,1 -n | cut -f2- -d' ' | grep -oh '.*refs/tags/[v0-9\.]*$' | tail -1 | awk '{ printf "%s # %s\n",$1,$2 }')
-    egrep -lRZ "uses: $action@" .github/workflows/ | xargs -0 -l sed -i -e "s|uses: $action@.*|uses: $action@$commit_hash|g"
+    # shellcheck disable=SC2267
+    grep -ElRZ "uses: $action@" .github/workflows/ | xargs -0 -l sed -i -e "s|uses: $action@.*|uses: $action@$commit_hash|g"
 done
