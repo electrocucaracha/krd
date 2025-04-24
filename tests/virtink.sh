39c3ad2830de2041b42528892f3f7edecabd13eb
Victor Morales <chipahuac@hotmail.com>
2025-04-24 13:14:23 -0700

    Improve virtlink tests

--- tests/virtink.sh
+++ tests/virtink.sh
@@ -17,55 +17,19 @@ source _functions.sh
 # shellcheck source=tests/_assertions.sh
 source _assertions.sh
 
-vm_name=ubuntu-container-rootfs
-
 function cleanup {
-    kubectl delete vm $vm_name --ignore-not-found
+    kubectl delete -f resources/virtink/
 }
 
 # Setup
-# editorconfig-checker-disable
-cat <<EOF | kubectl apply -f -
-apiVersion: virt.virtink.smartx.com/v1alpha1
-kind: VirtualMachine
-metadata:
-  name: $vm_name
-spec:
-  instance:
-    memory:
-      size: 1Gi
-    kernel:
-      image: smartxworks/virtink-kernel-5.15.12
-      cmdline: "console=ttyS0 root=/dev/vda rw"
-    disks:
-      - name: ubuntu
-      - name: cloud-init
-    interfaces:
-      - name: pod
-  volumes:
-    - name: ubuntu
-      containerRootfs:
-        image: smartxworks/virtink-container-rootfs-ubuntu
-        size: 4Gi
-    - name: cloud-init
-      cloudInit:
-        userData: |-
-          #cloud-config
-          password: password
-          chpasswd: { expire: False }
-          ssh_pwauth: True
-  networks:
-    - name: pod
-      pod: {}
-EOF
-# editorconfig-checker-enable
-sleep 5
+kubectl apply -f resources/virtink/
 trap cleanup EXIT
 
 # Test
 info "===== Test started ====="
 
-kubectl wait vm ubuntu-container-rootfs --for jsonpath='{.status.phase}'=Running --timeout -1s
+vm_name="ubuntu-container-rootfs"
+kubectl wait vm "$vm_name" --for jsonpath='{.status.phase}'=Running --timeout -60s
 pod_name=$(kubectl get vm "$vm_name" -o jsonpath='{.status.vmPodName}')
 assert_are_not_equal "$(kubectl run "ssh-$vm_name" --rm --image=alpine --restart=Never -it -- uname -a)" "$(uname -a)" "$pod_name has the same kernel version than the host"
 
