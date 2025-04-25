4cc994d3d34fb81408781d002a60e8b7aa759f38
Victor Morales <chipahuac@hotmail.com>
2025-04-25 10:40:45 -0700

    Centralize KRD_DEBUG definition

--- ci/provision_installer.sh
+++ ci/provision_installer.sh
@@ -17,7 +17,7 @@ source _common.sh
 
 if [ "$($VAGRANT_CMD status installer | grep "^installer" | awk '{ print $2}')" != "running" ]; then
     if [[ ${HOST_INSTALLER:-false} == "true" ]]; then
-        KRD_DEBUG=true ./krd_command.sh -a install_k8s
+        ./krd_command.sh -a install_k8s
     else
         $VAGRANT_CMD_UP installer
     fi
